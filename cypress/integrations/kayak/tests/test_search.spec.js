c760977606036feebce81afac370faf77170453d
Afraz Malik <afrazmalik321@gmail.com>
2024-03-17 15:45:35 +0500

    Cleaning

--- cypress/integrations/kayak/tests/test_search.spec.js
+++ cypress/integrations/kayak/tests/test_search.spec.js
@@ -97,8 +97,6 @@ describe("Search Tests", () => {
     // Click on the cheapest tab
     search_result.getCheapest().click();
 
-    let secondCurrencyName, storeThirdCurrency;
-
     cy.wait(10000); // If we remove this, we get $remoteJquery not defined at .then
 
     // Get the first cheap price card from results
@@ -146,110 +144,21 @@ describe("Search Tests", () => {
                     let currentSelectedPriceRate = JSON.stringify(
                       apiResponse.body.rates[key]
                     );
-                    var currentSelectedConvertedPrice =
+                    var currentSelectedConvertedPriceInUSD =
                       rate_Conversion.rateConversion(
                         currentSelectedPriceRate,
                         currentSelectedPrice
                       );
                     cy.log("in then second card price is ", price2, uniqueId);
-                    expect(parseInt(currentSelectedConvertedPrice)).to.equal(
-                      parseInt(USDPrice)
-                    );
+                    expect(
+                      parseInt(currentSelectedConvertedPriceInUSD)
+                    ).to.equal(parseInt(USDPrice));
                   });
                 cy.clearCookies();
               });
             });
         }
       );
-      // Store the currency of USD price of unique card in variable USDPrice
-      // cy.get(`[data-resultid=${uniqueId}]`)
-      //   .find(".f8F1-price-text")
-      //   .eq(0)
-      //   .then((PriceVal) => {
-      //     USDPrice = PriceVal.text();
-      // select second currency name and store its value
-      // search_result.openCurrencyModal();
-      // cy.get(":nth-child(2) > :nth-child(2) > .KmfS").then((text) => {
-      //   secondCurrencyName = text.text();
-      // });
-      // search_result.saveSCurrency().click({ force: true });
-      // cy.wait(10000); // here we wait for page to reload
-      // search_result.getCheapest().click(); // sometimes when page reloads, it goes back to "Best" tab, so we have to click on "Cheapest" tab
-      // cy.get(`[data-resultid=${uniqueId}]`)
-      //   .find(".f8F1-price-text")
-      //   .eq(0)
-      //   .then((PriceVal) => {
-      //     price2 = PriceVal.text();
-      //     cy.log("in then second card price is ", price2, uniqueId);
-      //   });
-      // cy.clearCookies();
-
-      // // select third currency and store its value
-      // search_result.openCurrencyModal();
-      // cy.get(":nth-child(2) > :nth-child(3) > .KmfS").then((text) => {
-      //   storeThirdCurrency = text.text();
-      // });
-      // search_result.saveTCurrency().click({ force: true });
-      // cy.wait(10000); // here we wait for page to reload
-      // search_result.getCheapest().click(); // sometimes when page reloads, it goes back to "Best" tab, so we have to click on "Cheapest" tab
-      // cy.get(`[data-resultid=${uniqueId}]`)
-      //   .find(".f8F1-price-text")
-      //   .eq(0)
-      //   .then((PriceVal) => {
-      //     price3 = PriceVal.text();
-      //     cy.log("in then third card price is ", price3, uniqueId);
-      //   });
-
-      // //  compare rates using rates API
-      // cy.request("GET", "https://open.er-api.com/v6/latest/USD").then(
-      //   (response) => {
-      //     // Assertions on the response
-      //     expect(response.status).to.equal(200);
-      //     APIResponse = response;
-      //     cy.log(
-      //       USDPrice,
-      //       price2,
-      //       price3,
-      //       secondCurrencyName,
-      //       storeThirdCurrency
-      //     );
-      //     // Perform assertion: Compare payload USD rate with response
-      //     let USDrate = JSON.stringify(response.body.rates.USD);
-      //     let EURrate = JSON.stringify(response.body.rates.EUR);
-      //     let CADrate = JSON.stringify(response.body.rates.CAD);
-      //     cy.log("Rates from API are: ", USDrate, EURrate, CADrate);
-      //     cy.log(USDPrice, price2, price3);
-      //     var USDprice = rate_Conversion.extractPriceValue(USDPrice);
-      //     var EURprice = rate_Conversion.extractPriceValue(price2);
-      //     var CADprice = rate_Conversion.extractPriceValue(price3);
-      //     cy.log("DOM Prices are: ", USDprice, EURprice, CADprice);
-      //     var USDconvertedPrice = rate_Conversion.rateConversion(
-      //       USDrate,
-      //       USDprice
-      //     );
-      //     var EURconvertedPrice = rate_Conversion.rateConversion(
-      //       EURrate,
-      //       USDprice
-      //     );
-      //     var CADconvertedPrice = rate_Conversion.rateConversion(
-      //       CADrate,
-      //       USDprice
-      //     );
-
-      //     cy.log(
-      //       "Flight prices after conversion from the API are: ",
-      //       USDconvertedPrice,
-      //       EURconvertedPrice,
-      //       CADconvertedPrice
-      //     );
-
-      //     // assertion to compare DOM Rates with Conversion API rates
-      //     expect(USDprice).to.equal(USDconvertedPrice);
-      //     expect(EURprice).to.equal(EURconvertedPrice);
-      //     expect(CADprice).to.equal(CADconvertedPrice);
-      //   }
-      // );
-      // });
     });
   });
 });
